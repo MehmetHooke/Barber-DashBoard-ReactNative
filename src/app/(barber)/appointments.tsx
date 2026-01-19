693bdb373b0b27dba5f90af8e386117cfb22351b
MehmetHooke <hoke628@gmail.com>
2026-01-19 21:36:30 +0300

    Plan for upcoming updates

--- src/app/(barber)/appointments.tsx
+++ src/app/(barber)/appointments.tsx
@@ -1,11 +1,11 @@
 import { Text, View } from "react-native";
 
-
 export default function book() {
-
-    return (
-        <View className='flex justify-center justify-items-center w-full h-full'>
-            <Text className='text-center text-2xl'>Randevularım</Text>
-        </View>
-    )
+  return (
+    <View className="flex justify-center justify-items-center w-full h-full">
+      <Text className="text-center text-2xl">
+        Randevularım berberin adına alınan randevular burada gözükücek
+      </Text>
+    </View>
+  );
 }
--- src/app/(barber)/dashboard.tsx
+++ src/app/(barber)/dashboard.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { Text, View } from "react-native";
 
 const dashboard = () => {
   return (
-        <View className='flex justify-center justify-items-center w-full h-full'>
-            <Text className='text-center text-2xl'>dashboard</Text>
-        </View>
-  )
-}
+    <View className="flex justify-center justify-items-center w-full h-full">
+      <Text className="text-center text-2xl">
+        dashboard bu kısım tüm veri işlemleri bittikten sonra yapılacak
+      </Text>
+    </View>
+  );
+};
 
-export default dashboard
+export default dashboard;
